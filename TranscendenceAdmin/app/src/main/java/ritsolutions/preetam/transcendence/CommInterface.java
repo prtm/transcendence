92d0c849ca729fec63e407c6dfff20892d60897b
PREETAM SWAMI <prtmswami@gmail.com>
2017-02-27 02:41:18 +0530

    Naming Convention Corrected

---
+++ TranscendenceAdmin/app/src/main/java/ritsolutions/preetam/transcendence/CommInterface.java
@@ -0,0 +1,8 @@
+package ritsolutions.preetam.transcendence;
+
+/**
+ * Created by Ghost on 26-Sep-16.
+ */
+public interface Communicator {
+    void stopProgressBar();
+}
--- TranscendenceAdmin/app/src/main/java/ritsolutions/preetam/transcendence/Communicator.java
+++
@@ -1,8 +0,0 @@
-package ritsolutions.preetam.transcendence;
-
-/**
- * Created by Ghost on 26-Sep-16.
- */
-public interface Communicator {
-    void stopProgressBar();
-}
